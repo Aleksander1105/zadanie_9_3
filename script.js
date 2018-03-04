8c96cc0dee3cfdd59ae509c56bba21165b78fe58
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-03-04 12:48:38 +0100

    script.js ready to check

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 var dinosaur = 'triceratops';
 var capitalLetters = dinosaur.toUpperCase();
 var substitute = text.replace('Velociraptor', capitalLetters);
-var toSlice = substitute.substr(0,72);
+var toSlice = substitute.substr(0, substitute.length/2);
 
 
 console.log(toSlice);
